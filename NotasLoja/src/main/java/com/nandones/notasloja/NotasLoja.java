b39a017a694af75488a04f1efde1bd339c66c2e6
nandones <fernandovedana03@gmail.com>
2024-03-17 14:15:56 -0300

    arquivo java

---
+++ NotasLoja/src/main/java/com/nandones/notasloja/NotasLoja.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nandones.notasloja;
+
+/**
+ *
+ * @author Fernando
+ */
+public class NotasLoja {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
