c0fbef280deb6a5e09b43c4f41c154c51dc52b1c
nandones <fernandovedana03@gmail.com>
2024-04-03 17:55:42 -0300

    (pedidoDAO) (ARREDONDADOISDIGITOS)

--- NotasLoja/src/main/java/com/miguelefernando/notasloja/utils/ArredondaDoisDigitos.java
+++ NotasLoja/src/main/java/com/miguelefernando/notasloja/utils/ArredondaDoisDigitos.java
@@ -2,15 +2,9 @@ package com.miguelefernando.notasloja.utils;
 
 public class ArredondaDoisDigitos {
 
-    public static double truncarParaDuasCasas(double numero) {
+    public static double arredondarParaDuasCasas(double numero) {
         return Math.round(numero * 100.0) / 100.0;
     }
 
-    public static void main(String[] args) {
-        double numero = 12.3456;
-        double numeroTruncado = truncarParaDuasCasas(numero);
-        System.out.println("Número original: " + numero);
-        System.out.println("Número truncado: " + numeroTruncado);
-    }
 
 }
