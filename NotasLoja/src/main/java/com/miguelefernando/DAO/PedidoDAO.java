132fa89f334a96d6a1cb4c46ed0b8ac0dc0673f8
nandones <fernandovedana03@gmail.com>
2024-04-07 10:39:40 -0300

    {ArredondaDoisDigitos } DELETED

--- NotasLoja/src/main/java/com/miguelefernando/DAO/PedidoDAO.java
+++ NotasLoja/src/main/java/com/miguelefernando/DAO/PedidoDAO.java
@@ -1,7 +1,7 @@
 package com.miguelefernando.DAO;
 
 import java.sql.Connection;
-import java.sql.Date; // Importe a classe java.sql.Date
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
